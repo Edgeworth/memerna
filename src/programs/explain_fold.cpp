9abbd5c4135a53fe3736e4b1c453a3d65ee824e8
E <edgeworth.dev@gmail.com>
2016-08-24 00:16:50 +0800

    Fix small bug in InitFold (only happens on random energy models).

--- src/programs/explain_fold.cpp
+++ src/programs/explain_fold.cpp
@@ -8,13 +8,15 @@ using namespace memerna;
 
 int main(int argc, char* argv[]) {
   ArgParse argparse(energy::ENERGY_OPTIONS);
+  argparse.AddOptions(fold::FOLD_OPTIONS);
   argparse.ParseOrExit(argc, argv);
   auto pos = argparse.GetPositional();
   verify_expr(pos.size() == 4, "need primary sequence and dp table indices");
 
   energy::LoadEnergyModelFromArgParse(argparse);
   fold::fold_state_t state;
-  fold::Fold0(parsing::StringToRna(pos[0]), &state);
+  auto fold_fn = fold::FoldFunctionFromArgParse(argparse);
+  fold_fn(parsing::StringToRna(pos[0]), &state);
 
   int st = atoi(pos[1].c_str()), en = atoi(pos[2].c_str()), a = atoi(pos[3].c_str());
   fold::traceback_stack_t q;
