d0f1299c01fcf940a35b42d29d46ba45b9a9d2c8
E <edgeworth.dev@gmail.com>
2016-10-27 17:23:23 +0800

    Fix convert script.

--- scripts/convert.py
+++ scripts/convert.py
@@ -23,12 +23,14 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-t', '--to', choices=['db', 'ct'], required=True)
   parser.add_argument('-f', '--filename', type=str)
-  parser.add_argument('data', type=str, nargs='2', required=False)
+  parser.add_argument('data', type=str, nargs='*')
   args = parser.parse_args()
 
   if args.filename:
     rna = RNA.from_any_file(read_file(args.filename))
   else:
+    if len(args.data) != 2:
+      parser.error('require primary sequence and dot-bracket')
     rna = RNA.from_name_seq_db('user', *args.data)
   if args.to == 'ct':
     print(rna.to_ct_file())
