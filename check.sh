ccd106974b35e0c0f6784eed641f527a87cc6a85
Eliot Courtney <edgeworth.dev@gmail.com>
2022-01-04 11:23:51 +0900

    use gnu parallel for build check script

--- check.sh
+++ check.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-set -xe  # exit on first failure and print commands
+set -xe  # exit on failure and print commands
 
 # Check various build configurations and run tests. Good to run before push.
-for type in debug relwithdebinfo
-do
-  ./build.py --type $type
-  ./build.py --type $type --use-rnastructure
-  ./build.py --type $type --use-mpfr
-  ./build.py --type $type --use-rnastructure --use-mpfr
-  ./build.py --type $type --use-clang
-  ./build.py --type $type --use-clang --use-rnastructure
-  ./build.py --type $type --use-clang --use-mpfr
-  ./build.py --type $type --use-clang --use-rnastructure --use-mpfr
-done
+parallel --progress --load 100% ./build.py --type ::: debug relwithdebinfo ::: '' --use-rnastructure ::: '' --use-mpfr ::: '' --use-clang
