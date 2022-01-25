88cb1080aa397c249befb3b68f03ce37a78009ad
Eliot Courtney <edgeworth.dev@gmail.com>
2022-01-25 20:43:10 +0900

    Add stacktrace functionality if boost is available

--- src/util/error.h
+++ src/util/error.h
@@ -7,6 +7,20 @@
 
 #include "util/string.h"
 
+#ifdef USE_BOOST
+#include <boost/stacktrace.hpp>
+// Like assert, but can't be disabled.
+#define verify(expr, ...)                                                   \
+  do {                                                                      \
+    if (!(expr)) {                                                          \
+      auto msg = ::mrna::sfmt("%s:%d: ", __func__, __LINE__);               \
+      msg += ::mrna::sfmt(__VA_ARGS__);                                     \
+      msg += '\n';                                                          \
+      msg += boost::stacktrace::to_string(boost::stacktrace::stacktrace()); \
+      throw ::std::runtime_error(msg);                                      \
+    }                                                                       \
+  } while (0)
+#else
 // Like assert, but can't be disabled.
 #define verify(expr, ...)                                     \
   do {                                                        \
@@ -17,6 +31,7 @@
       throw ::std::runtime_error(msg);                        \
     }                                                         \
   } while (0)
+#endif
 
 #define error(...) verify(false, __VA_ARGS__)
 
