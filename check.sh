d6251e56d0a9fb595d75df81b9eca0718af97244
E <edgeworth.dev@gmail.com>
2022-01-25 12:19:21 +0900

    Fix sanitizer issues

--- check.sh
+++ check.sh
@@ -3,8 +3,9 @@
 set -xe  # exit on failure and print commands
 
 # Check various build configurations and run tests. Good to run before push.
+# Uses :::+ to match mpfr-rnastructure and no-mpfr-no-rnastructure
 parallel --progress --jobs 8 ./build.py --test {} ">" /dev/null ::: \
-  --type=debug --type=relwithdebinfo ::: --sanitizer=none --sanitizer=asan \
-  --sanitizer=msan --sanitizer=tsan --sanitizer=ubsan ::: \
-  --rnastructure --no-rnastructure ::: --mpfr --no-mpfr ::: \
+  --type=debug --type=relwithdebinfo ::: --sanitizer=asan \
+  --sanitizer=tsan --sanitizer=ubsan ::: \
+  --rnastructure --no-rnastructure :::+ --mpfr --no-mpfr ::: \
   --compiler=clang
