7b782df4a76d268dda9ac39249c2339b522ed3b8
Eliot Courtney <edgeworth.dev@gmail.com>
2022-11-04 22:48:18 +0900

    Plots

--- rnapy/analysis/fold_perf/plotter.py
+++ rnapy/analysis/fold_perf/plotter.py
@@ -5,12 +5,13 @@ import seaborn as sns
 import matplotlib as mpl
 
 from rnapy.analysis.metrics import Dataset
-from rnapy.analysis.plot.plots import Column
+from rnapy.analysis.plot.plots import Column, plot_mean_quantity
+from rnapy.analysis.plot.util import save_figure
 from rnapy.util.format import human_size
 
 
 class FoldPerfPlotter:
-    COLMAP: dict[str, Column] = {
+    COLS: dict[str, Column] = {
         "name": Column(idx="name", name="Name"),
         "length": Column(idx="length", name="Length (nuc)"),
         "real_sec": Column(idx="real_sec", name="Wall time (s)"),
@@ -30,14 +31,24 @@ class FoldPerfPlotter:
         self.output_dir = output_dir
         sns.set(color_codes=True)
 
-    def load_datasets(self) -> dict[str, Dataset]:
+    def _load_datasets(self) -> dict[str, Dataset]:
         datasets: dict[str, Dataset] = {}
         for path in self.input_dir.iterdir():
-            dataset, program = path.stem.split("_")
+            dataset, program = path.stem.rsplit("_", maxsplit=1)
             df = pd.read_csv(path)
             datasets.setdefault(dataset, Dataset(dataset))
             datasets[dataset].dfs[program] = df
         return datasets
 
+    def _path(self, ds: Dataset, name: str) -> Path:
+        return self.output_dir / f"{ds.name}_{name}.png"
+
+    def _plot(self, ds: Dataset) -> None:
+        f = plot_mean_quantity(ds, self.COLS["length"], self.COLS["real_sec"])
+        save_figure(f, self._path(ds, "test"))
+
     def run(self) -> None:
-        ds = self.load_datasets()
+        datasets = self._load_datasets()
+        for ds in datasets.values():
+            self._plot(ds)
+            break
