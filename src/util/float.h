24d472a0dd393a205267d1fdc9a944b7aaa09b7b
Eliot Courtney <edgeworth.dev@gmail.com>
2023-01-16 16:44:34 +0900

    Fix mpfr formatting for fmtlib.

--- src/util/float.h
+++ src/util/float.h
@@ -7,7 +7,10 @@
 #include <cstdint>
 
 #ifdef USE_MPFR
+#include <fmt/ostream.h>
+
 #include <boost/multiprecision/mpfr.hpp>
+
 namespace boost {
 
 inline void throw_exception(const std::exception& e) {
@@ -39,7 +42,10 @@ using flt = long double;
 static_assert(false, "unknown float bits")
 #endif
 
+// Redefinitions to use std:: for long double support, but also to keep
+// MPFR happy.
 inline flt fabs(flt v) { return std::abs(v); }
+inline flt exp(flt v) { return std::exp(v); }
 
 #endif  // USE_MPFR
 
@@ -60,5 +66,39 @@ inline bool rel_eq(flt a, flt b, flt rel = EP) {
 }
 
 }  // namespace mrna
+
+#ifdef USE_MPFR
+
+namespace fmt {
+template <>
+struct formatter<::mrna::flt> {
+ private:
+  detail::dynamic_format_specs<char> specs_;
+
+ public:
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+    using handler_type = detail::dynamic_specs_handler<format_parse_context>;
+    auto type = detail::type_constant<::mrna::flt, char>::value;
+    auto checker = detail::specs_checker<handler_type>(handler_type(specs_, ctx), type);
+    auto end = detail::parse_format_specs(ctx.begin(), ctx.end(), checker);
+    return end;
+  }
+
+  template <class FormatContext>
+  auto format(const ::mrna::flt& c, FormatContext& ctx) -> decltype(ctx.out()) {
+    std::stringstream out;
+    detail::handle_dynamic_spec<detail::precision_checker>(
+        specs_.precision, specs_.precision_ref, ctx);
+    if (specs_.precision > 0) out << std::setprecision(specs_.precision);
+    if (specs_.width > 0) out << std::setw(specs_.width);
+    if (specs_.type == presentation_type::fixed_lower) out << std::fixed;
+    if (specs_.type == presentation_type::exp_lower) out << std::scientific;
+    out << c;
+    return format_to(ctx.out(), "{}", out.str());
+  }
+};
+}  // namespace fmt
+
+#endif  // USE_MPFR
 
 #endif  // UTIL_FLOAT_H_
