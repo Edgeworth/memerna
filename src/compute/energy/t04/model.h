f2f382872e96527622623f1f5286d8f2f96ac66f
Eliot Courtney <edgeworth.dev@gmail.com>
2023-01-22 21:12:18 +0900

    test virtual 2

--- src/compute/energy/t04/model.h
+++ src/compute/energy/t04/model.h
@@ -27,32 +27,11 @@ namespace mrna::erg::t04 {
 
 class Model : public ModelMixin<Model>, public T04ModelMixin {
  public:
-  Energy Hairpin(const Primary& r, int st, int en, std::unique_ptr<Structure>* s = nullptr) const;
-  Energy Bulge(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy InternalLoop(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy TwoLoop(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy MultiloopEnergy(const Primary& r, const Secondary& s, int st, int en,
-      std::deque<int>* branches, bool use_given_ctds, Ctds* ctd,
-      std::unique_ptr<Structure>* sstruc = nullptr) const;
-
-  // If (st, en) is not paired, treated as an exterior loop.
-  // If |ctd| is non-null, use the given ctds.
-  EnergyResult SubEnergy(const Primary& r, const Secondary& s, const Ctds* given_ctd, int st,
-      int en, bool build_structure = false) const;
-  EnergyResult TotalEnergy(const Primary& r, const Secondary& s, const Ctds* given_ctd,
-      bool build_structure = false) const;
-
  private:
   friend class ModelMixin<Model>;
 
   // This is private to prevent construction on the stack, since this structure is large.
   Model() = default;
-
-  Energy SubEnergyInternal(const Primary& r, const Secondary& s, int st, int en,
-      bool use_given_ctds, Ctds* ctd, std::unique_ptr<Structure>* struc) const;
 };
 
 }  // namespace mrna::erg::t04
--- src/compute/energy/t22/model.h
+++ src/compute/energy/t22/model.h
@@ -30,24 +30,6 @@ class Model : public ModelMixin<Model>, public T04ModelMixin {
  public:
   Energy penultimate_stack[4][4][4][4] = {};
 
-  Energy Hairpin(const Primary& r, int st, int en, std::unique_ptr<Structure>* s = nullptr) const;
-  Energy Bulge(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy InternalLoop(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy TwoLoop(const Primary& r, int ost, int oen, int ist, int ien,
-      std::unique_ptr<Structure>* s = nullptr) const;
-  Energy MultiloopEnergy(const Primary& r, const Secondary& s, int st, int en,
-      std::deque<int>* branches, bool use_given_ctds, Ctds* ctd,
-      std::unique_ptr<Structure>* sstruc = nullptr) const;
-
-  // If (st, en) is not paired, treated as an exterior loop.
-  // If |ctd| is non-null, use the given ctds.
-  EnergyResult SubEnergy(const Primary& r, const Secondary& s, const Ctds* given_ctd, int st,
-      int en, bool build_structure = false) const;
-  EnergyResult TotalEnergy(const Primary& r, const Secondary& s, const Ctds* given_ctd,
-      bool build_structure = false) const;
-
  protected:
   void LoadFromDir(const std::string& data_dir);
 
@@ -58,9 +40,6 @@ class Model : public ModelMixin<Model>, public T04ModelMixin {
 
   // This is private to prevent construction on the stack, since this structure is large.
   Model() = default;
-
-  Energy SubEnergyInternal(const Primary& r, const Secondary& s, int st, int en,
-      bool use_given_ctds, Ctds* ctd, std::unique_ptr<Structure>* struc) const;
 };
 
 }  // namespace mrna::erg::t22
