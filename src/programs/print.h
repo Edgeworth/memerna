cf0e2108e229ca9bd348dd0ad95ed5d17095f52a
Eliot Courtney <edgeworth.dev@gmail.com>
2024-09-25 23:11:42 +0900

    Add extra partition tables

--- src/programs/print.h
+++ src/programs/print.h
@@ -2,10 +2,20 @@
 #ifndef PROGRAMS_PRINT_H_
 #define PROGRAMS_PRINT_H_
 
+#include "backends/common/base/model_base.h"
 #include "model/pfn.h"
+#include "model/primary.h"
 
-void PrintBoltzProbs(const mrna::BoltzProbs& p);
+namespace mrna {
 
-void PrintPfn(const mrna::BoltzSums& p);
+void PrintBoltzProbs(const BoltzProbs& p);
+
+void PrintPfn(const BoltzSums& p);
+
+void PrintInnerStackProbs(const Primary& r, const PfnTables& pfn, const md::base::ModelBase& model);
+
+void PrintHelixProbs(const Primary& r, const PfnTables& pfn, const md::base::ModelBase& model);
+
+}  // namespace mrna
 
 #endif  // PROGRAMS_PRINT_H_
